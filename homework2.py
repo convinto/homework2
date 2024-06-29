1bc17b1f4dc3eabadffcaaa85cfe3853498711f6
convinto <convinto@yandex.ru>
2024-06-30 00:29:49 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_completed = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent/homework_completed
+print('Курс:', course_name, ',', 'всего задач:', homework_completed,', ', 'затрачено часов:', hours_spent, ',', 'среднее время выполнения', time_per_task, 'часа.')
